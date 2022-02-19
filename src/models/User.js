3e614d24b2a74bbfb576415cec74cbc7b4850f57
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-19 15:01:33 -0300

    feat: :sparkles: creating user model

---
+++ src/models/User.js
@@ -0,0 +1,26 @@
+import { Schema, model } from 'mongoose'
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    bio: String,
+    avatar: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+)
+
+export default model('User', UserSchema)
