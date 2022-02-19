e7dea1501f6d85110d55134b1e20570b84e58dcb
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-02-19 17:33:16 -0300

    feat: :sparkles: updatin user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,5 @@
 import User from '../models/User'
+import bcrypt from 'bcrypt'
 
 class UserController {
   async store(request, response) {
@@ -21,6 +22,35 @@ class UserController {
     delete userResult['password']
 
     return response.status(201).json(userResult)
+  }
+
+  async update(request, response) {
+    const { name, email, oldPassword, password, bio, avatar } = request.body
+    const user = await User.findOne({ _id: request.userId })
+
+    if (!user) {
+      return response.status(404).json({ error: 'User not found.' })
+    }
+
+    if (oldPassword) {
+      if (!(await bcrypt.compare(oldPassword, user.password))) {
+        return response
+          .status(400)
+          .json({ error: 'Old password does no match.' })
+      }
+      user.password = password
+    }
+    user.name = name
+    user.email = email
+    user.bio = bio
+    user.avatar = avatar
+    user.save()
+
+    const { id, createdAt, updateAt } = user
+
+    return response
+      .status(201)
+      .json({ id, name, email, bio, avatar, createdAt, updateAt })
   }
 }
 
